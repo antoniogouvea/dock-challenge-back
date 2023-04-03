4122a1317d164b8fb1e393b99f31c10b9ab696a0
Antonio Gouvea <antonio.coutinho.gouvea@gmail.com>
2023-04-03 18:44:22 -0300

    ci: :green_heart: create ci for aws

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ import { AppModule } from './app.module'
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	const configService = app.get(ConfigService)
-	const port = configService.get('PORT')
+	const port = process.env.PORT
 	app.useGlobalPipes(new ValidationPipe())
 	app.enableCors({
 		origin: '*',
 	})
-	await app.listen(port)
+	await app.listen(port || 3000)
 	console.log(`Application is running on: ${await app.getUrl()}`)
 }
 bootstrap()
